0340418abd49e4105a4bfe26bb1d50cf697bc447
Muhammad Habibullah <121166723+habibhaseeb@users.noreply.github.com>
2024-07-09 18:48:48 +0500

    Create converting_bw_datatypes.py

---
+++ Practices/converting_bw_datatypes.py
@@ -0,0 +1,29 @@
+         #                 // Converting an integer String
+a = '19748';
+b = int(a);
+print(b);
+
+checker = type(b);
+print(checker);
+
+      #      // Converting a Float String...
+
+number1 = '12.8965';
+number2 = '0.79711314';
+
+checker1 = float (number1) , float(number2); # This is float datatype
+
+checker2 = list(checker1);
+
+convert = set(number1);
+
+print(convert)
+
+print(checker1, checker2);
+
+# converting string to dictionary
+
+import json
+json.loads(number1)
+print(number1);
+
