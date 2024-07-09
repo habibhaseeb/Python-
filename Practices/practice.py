82c3884ba0954effba2d7c0cf3de2a7cf0322e2c
Muhammad Habibullah <121166723+habibhaseeb@users.noreply.github.com>
2024-07-09 18:50:49 +0500

    Create practice.py

---
+++ Practices/practice.py
@@ -0,0 +1,29 @@
+num1 = 10
+num2 = 12
+# print(x);
+ 
+
+string = "Habibullah";
+formated_str = reversed('hello');
+ 
+# print(formated_str);
+# print(type (formated_str));
+
+# list in python is not hashable (mutable) like you change reference
+
+elem = [1, 2, 3 , 'python' ,( 45, 56)];
+elem[3] = "something else" # not TypeError
+
+print(elem); 
+
+# There are many methods in python of list to be customized
+# Lists are also known as Arrays in other programming languages.
+
+anotherList = [10.e2];
+elem.append(anotherList);
+print(elem)
+
+# set 
+a = {1 , 2 , 'a'}
+b = None
+print(a , b)
