3205690716f5abf27ecd3282effbdca5d75652d7
Muhammad Habibullah <121166723+habibhaseeb@users.noreply.github.com>
2024-07-10 20:00:28 +0500

    Create Strings.py

---
+++ Datatypes/Strings.py
@@ -0,0 +1,16 @@
+normal = 'foo\nbar'
+print(normal);
+
+emptyList = []
+
+normal = list("abcde");
+emptyList = emptyList.append(normal)
+
+print(normal);
+
+string = "habib\\nullah";
+print(string);
+
+string = r"habib\nullah"     # ->>> without any interpretation of escape sequence
+    # r this is raw
+print(string);
