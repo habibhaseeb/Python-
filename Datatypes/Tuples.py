2be39c09bb81e1b0f0a6c23121edbbde1d72752f
Muhammad Habibullah <121166723+habibhaseeb@users.noreply.github.com>
2024-07-10 20:01:02 +0500

    Create Tuples.py

---
+++ Datatypes/Tuples.py
@@ -0,0 +1,15 @@
+names = ("habib","haseeb","ahmad","ali","hassan","john");
+values = (10,20,30,45,105);
+
+def print_names(position):
+    print("Printed:" , names)
+print(type(names));
+
+print_names(print(names[-6]));
+
+IPaddress = ('10.25.20.108', 8080);
+
+print("Your IP Address is :\n" , IPaddress[0]);
+print(values[1:4]);      # This syntax implies that 1:4 that starting value one and 
+                    # ending value is 4 but ignored one value at end
+        
